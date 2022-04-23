dd9b4b6f1ab36dfa80f8060a5f68ee40ac6e4be7
Axel Eschholz <axelion737@gmail.com>
2022-04-23 15:10:52 -0300

    initialized api

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectID;
 const CONNECTION_URL =
-  "mongodb://mongodb://ubmm8wsjyz30hhcsinm2:XSNioWk3sKa2K9f0TVUR@ble9ddg2vn3ijr8-mongodb.services.clever-cloud.com:27017/ble9ddg2vn3ijr8:WvdzcfnOdCajxYzvpbSI@bnsp132kjcfnyw1-mongodb.services.clever-cloud.com:27017/bnsp132kjcfnyw1";
+  "mongodb://ubmm8wsjyz30hhcsinm2:XSNioWk3sKa2K9f0TVUR@ble9ddg2vn3ijr8-mongodb.services.clever-cloud.com:27017/ble9ddg2vn3ijr8";
 const DATABASE_NAME = "ble9ddg2vn3ijr8";
 
 app.use(bodyParser.json());
@@ -15,24 +15,9 @@ app.use(cors());
 
 var database, collection;
 
-//GET customers
-app.get("/api/customers", (req, res) => {
-  database
-    .collection("customer")
-    .find({})
-    .toArray((error, result) => {
-      if (error) {
-        return res.status(500).send(error);
-      }
-      res.send(result);
-    });
-});
-
-//GET customer
-app.get("/api/customers/:id", (req, res) => {
-  var id = String(req.params.id);
-
-  collection.findOne({ customer_id: id }, (error, result) => {
+//GET books
+app.get("/api/books", (req, res) => {
+  collection.find({}).toArray((error, result) => {
     if (error) {
       return res.status(500).send(error);
     }
@@ -40,9 +25,21 @@ app.get("/api/customers/:id", (req, res) => {
   });
 });
 
-//POST customer
-app.post("/api/customers", (req, res) => {
-  database.collection("customer").insert(req.body, (error, result) => {
+//GET book
+app.get("/api/books/:id", (req, res) => {
+  var id = String(req.params.id);
+
+  collection.findOne({ bookID: id }, (error, result) => {
+    if (error) {
+      return res.status(500).send(error);
+    }
+    res.send(result);
+  });
+});
+
+//POST book
+app.post("/api/books", (req, res) => {
+  collection.insert(req.body, (error, result) => {
     if (error) {
       return res.status(500).send(error);
     }
@@ -50,27 +47,25 @@ app.post("/api/customers", (req, res) => {
   });
 });
 
-//PUT customer
-app.put("/api/customers/:id", (req, res) => {
+//PUT book
+app.put("/api/books/:id", (req, res) => {
   var id = String(req.params.id);
 
-  var myquery = { customer_id: id };
+  var myquery = { bookID: id };
   var newvalues = { $set: req.body };
-  database
-    .collection("customer")
-    .updateOne(myquery, newvalues, function (error, result) {
-      if (error) {
-        return res.status(500).send(error);
-      }
-      res.send(result.result);
-    });
+  collection.updateOne(myquery, newvalues, function (error, result) {
+    if (error) {
+      return res.status(500).send(error);
+    }
+    res.send(result.result);
+  });
 });
 
-//DELETE customer
-app.delete("/api/customers/:id", (req, res) => {
+//DELETE book
+app.delete("/api/books/:id", (req, res) => {
   var id = String(req.params.id);
 
-  var myquery = { customer_id: id };
+  var myquery = { bookID: id };
   collection.deleteOne(myquery, function (error, result) {
     if (error) {
       return res.status(500).send(error);
@@ -91,7 +86,7 @@ app.listen(5000, () => {
       collection = database.collection("books");
       console.log("Connected to `" + DATABASE_NAME + "`!");
       console.log(
-        "Listening on localhost:5000, try http://localhost:5000/api/customers"
+        "Listening on localhost:5000, try http://localhost:5000/api/books"
       );
     }
   );
